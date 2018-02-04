1d428c27de6f0e5677c246c917b525e3c607da8f
Jimmy Wong <tszming.wong@gmail.com>
2018-02-04 18:18:17 +0800

    Play around with Restful service

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -6,6 +6,8 @@ from rest_framework.decorators import api_view
 from rest_framework import views, serializers, status, viewsets
 from rest_framework.response import Response
 
+from .models import Poll
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
@@ -17,11 +19,21 @@ def poll_get(request):
     })
 
 
+class PollSerializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+
+
 class PollViewSet(viewsets.ViewSet):
     def list(self, request):
-        return Response([1, 2, 3, 4, 5])
+        polls = Poll.objects.all()
+        serializer = PollSerializer(polls, many=True)
+        return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
+        polls = Poll.objects.all().filter(first_name="pk")
+        serializer = PollSerializer(polls)
+        serializer.is_valid(raise_exception=True)
         return Response({"aaa": "bbb"})
 
     def create(self, request):
