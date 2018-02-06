df8bd0ebb15dcfef6ad422c1cdbeeed24d66eaf4
Jimmy Wong <tszming.wong@gmail.com>
2018-02-06 20:39:54 +0800

    Add some dummy Shit

--- backend/polls/models.py
+++ backend/polls/models.py
@@ -5,3 +5,19 @@ from django.db import models
 class Poll(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+
+class Vehicle(models.Model):
+    pass
+
+
+class Car(Vehicle):
+    pass
+
+
+class Truck(Vehicle):
+    pass
+
+
+class Wheel(models.Model):
+    pass
